614c8889a6dcef10bf79a6fbe4c1755f3a60b715
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-05-28 16:13:28 +0530

    Added Login and Signup api

--- Frontend/src/pages/LoginCard.jsx
+++ Frontend/src/pages/LoginCard.jsx
@@ -1,12 +1,49 @@
+import React,{useState} from "react";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+
 function LoginCard(){
+    const [Username,SetUsername] = useState("");
+    const [Password,SetPassword] = useState("");
+    const navigate = useNavigate();
+
+    const onsubmit = async()=> {
+        try {
+            const response = await axios.post("http://localhost:8000/api/login", {
+              Username,
+              Password
+            }, {
+              headers: {
+                'Content-Type': 'application/json'
+              }
+            });
+        // console.log(response);
+        navigate("/");
+
+        } catch (error) {
+          console.error("Login failed:", error.response?.data || error.message);
+        }
+    }
     return(
         <div className="login">
             <h1>Login Page</h1>
-            <input className="login-fields" type="text" placeholder="Enter Username"/>
-            <input className="login-fields" type="password" placeholder="Password"/>
-            <button className="submit-button">Submit</button>
+            <input 
+             className="login-fields" 
+             type="text" 
+             placeholder="Enter Username"
+             value = {Username}
+             onChange={(e)=>SetUsername(e.target.value)}
+            />
+            <input 
+             className="login-fields" 
+             type="password" 
+             placeholder="Password"
+             value={Password}
+             onChange={(e) => SetPassword(e.target.value)}
+            />
+            <button className="submit-button" onClick={onsubmit}>Submit</button>
         </div>
     );
 }
 
-export default LoginCard
+export default LoginCard;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,90 +1,20 @@
 import express from 'express';
 import cors from 'cors';
-import jwt from 'jsonwebtoken';
-import cookie from 'cookie';
-import bcrypt from 'bcryptjs';
-// import { PrismaClient } from '@prisma/client';
-import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import Connect from '../utils/db.js';
+import userRotuer from '../route/user.js';
 
-mongoose.connect('mongodb://localhost:27017/mydatabase');
-
-import User from '../model/user'; 
-
-const secret = "mysecret";
+Connect();
 
 const app = express();
-
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+app.use(cookieParser());
 
-app.get('/login', (req, res) => {
-    const token = req.cookies.token;
-
-    if (!token) {
-        return res.status(401).json({
-            msg: "Unauthorized"
-        });
-    }
-
-    jwt.verify(token, secret, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({
-                msg: "Unauthorized"
-            });
-        }
-        res.json({
-            msg: "Login successful",
-            user: decoded
-        });
-    });
-    redirectUrl = req.query.redirectUrl || '/';
-    res.redirect(redirectUrl);
-});
-
-
-app.get('/logout', (_, res) => {
-    res.setHeader('Set-Cookie', cookie.serialize('token', '', {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        maxAge: 0, 
-        sameSite: 'strict',
-    }));
-    res.json({
-        msg: "Logout successful"
-    });
-}
-);
-
-app.post('/signup',async (req, res) => {
-    console.log(req.body);
-    const userData = req.body;
-    const token = jwt.sign(userData, secret, { expiresIn: '1h' });
-    res.setHeader('Set-Cookie', cookie.serialize('token', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        maxAge: 3600, 
-        sameSite: 'strict',
-    }));
-
-    const hashedPassword = bcrypt.hashSync(User.password, 10);
-
-    const newUser = {
-        username : User.username,
-        email: User.email,
-        password: hashedPassword,
-    }
-
-    const data = await User.create({
-        data: newUser
-    });
-
-    console.log(data);
-
-    res.json({
-        msg: "Signup successful",
-        user: newUser
-    });
-});
+app.use('/api', userRotuer);
 
 app.listen(8000,()=>{
     console.log("Server is Started")
