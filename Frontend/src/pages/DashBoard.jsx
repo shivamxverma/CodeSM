ffa34278adc3fabf6019ed804bafcb81c296dadc
shivamxverma <shivam.verma.shiva.99@gmail.com>
2025-05-29 20:17:56 +0530

    Made Problem Schema

--- Frontend/src/pages/DashBoard.jsx
+++ Frontend/src/pages/DashBoard.jsx
@@ -1,13 +1,14 @@
 import React,{useState,useEffect} from "react";
 import Navbar from '../component/NavBar';
-
+import { Link } from 'react-router-dom';
+// import Navigate from 'react-router-dom';
 
 const Dashboard = () => {
-
+    // const router = Navigate();
     return (
         <>
           <Navbar/>
-          <button>Create Contest</button>
+          {/* <Link><button>Create Problem</button></Link> */}
         </>
     );
 }
